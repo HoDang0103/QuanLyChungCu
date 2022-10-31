4b1f813e769eefdddf9aff5c37606eec016cb261
H1eu69 <anrayno200@gmail.com>
2022-10-31 22:34:32 +0700

    Added IDNumber, Gender, ApartmentID, Birthdate, OTP in CurrentUserProfileEditDto. Customized API to send OTP

--- src/Framework.Application.Shared/Authorization/Users/Profile/Dto/CurrentUserProfileEditDto.cs
+++ src/Framework.Application.Shared/Authorization/Users/Profile/Dto/CurrentUserProfileEditDto.cs
@@ -33,6 +33,8 @@ namespace Framework.Authorization.Users.Profile.Dto
         [MaxLength(UserConsts.MaxIDNumberLength)]
         public string IDNumber { get; set; }
 
+        public string ApartmentId { get; set; }
+
         [Required]
         public DateTime BirthDate { get; set; }
         public virtual bool IsPhoneNumberConfirmed { get; set; }
