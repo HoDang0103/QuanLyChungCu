84ae4886819b21a14539b9ea651fcd9bbe25d813
Khôi Trần Duy <20521483@ms.uit.edu.vn>
2022-11-02 21:42:06 +0700

    chinh sua 2

---
+++ src/Framework.Web.Host/Data/ApartmentDb.cs
@@ -0,0 +1,13 @@
+﻿using Framework.Web.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Framework.Web.Data
+{
+    public class ApartmentDb : DbContext
+    {
+        public ApartmentDb(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Apartment> AbpApartments { get; set; }
+    }
+}
